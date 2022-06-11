942d4abc648eedb9f3f145f689fff2448b3f56ff
Rohit <stylishrb711@gmail.com>
2022-06-11 23:55:48 +0530

    app update

--- contact-manager/src/App.jsx
+++ contact-manager/src/App.jsx
@@ -1,32 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import AddContact from "./components/AddContact";
 import ContactList from "./components/ContactList";
 import Navbar from "./components/Navbar";
 
 function App() {
-  const [contacts, setContacts] = useState([
-    {
-      id: 1,
-      name: "Thor",
-      email: "thor@gmail.com",
-    },
-    {
-      id: 2,
-      name: "Tony",
-      email: "tony@gmail.com",
-    },
-    {
-      id: 3,
-      name: "Hulk",
-      email: "hulk@gmail.com",
-    },
-  ]);
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([]);
 
   const addContactHandler = (contact) => {
     console.log(contact);
     setContacts([...contacts, contact]);
   };
+
+  useEffect(() => {
+    const getDataFromLS = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (getDataFromLS) {
+      setContacts(getDataFromLS);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
 
   return (
     <div className="App">
