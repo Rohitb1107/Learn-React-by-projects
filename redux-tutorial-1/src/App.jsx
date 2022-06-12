e436ac415168bb8e01d65b1e8bb21e8eae3b11fe
Rohit <stylishrb711@gmail.com>
2022-06-12 23:53:49 +0530

    action created

--- redux-tutorial-1/src/App.jsx
+++ redux-tutorial-1/src/App.jsx
@@ -2,26 +2,13 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
   return (
     <div className="App">
       <div>{count}</div>
       <div>
-        <button
-          onClick={() => {
-            setCount(count + 1);
-          }}
-        >
-          +
-        </button>
-
-        <button
-          onClick={() => {
-            setCount(count - 1);
-          }}
-        >
-          -
-        </button>
+        <button>+</button>
+        <button>-</button>
       </div>
     </div>
   );
