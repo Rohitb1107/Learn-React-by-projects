a180bf44708aea5cdd8db5bd4b04457d0d77609a
Rohit <stylishrb711@gmail.com>
2022-06-13 09:47:40 +0530

    action created

--- redux-tutorial-2/src/Redux/store.js
+++ redux-tutorial-2/src/Redux/store.js
@@ -1,4 +1,4 @@
-// import { createStore } from "redux";
+import { createStore } from "redux";
+import { reducer } from "./reducer";
 
-
-// export store = createStore()
+export const store = createStore(reducer);
